cae734f1d5519a27815a95e39b3f49308af5d8af
dam33534@naver.com <dam33534@naver.com>
2025-10-31 15:56:43 +0900

    빌드오류 수정
    빌드오류 수정

--- next.config.js
+++ next.config.js
@@ -1,10 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: process.env.NODE_ENV === 'development' ? undefined : 'export',
-  trailingSlash: true,
-  images: {
-    unoptimized: true
-  },
+  // Static export 제거 - Server Actions 사용을 위해
+  // Vercel 배포 시 static export는 필요하지 않습니다
   eslint: {
     dirs: ['pages', 'components', 'lib', 'src'],
     // 빌드 시 ESLint 무시 (개발 중에는 IDE에서 체크)
